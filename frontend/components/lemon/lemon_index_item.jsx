bf460f89d3fe34756d17e9c709e0ebed4356cdda
angrobertsh <angrobertsh@gmail.com>
2020-07-19 13:49:09 -0700

    edit mode and font

--- frontend/components/lemon/lemon_index_item.jsx
+++ frontend/components/lemon/lemon_index_item.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const LemonIndexItem = ({lemon, setFocus, editLemon, deleteLemon}) => {
+  let editModeCheck = sessionStorage.getItem("robertslemonpartykey") === 'xe8v6pai7ct';
+
   let icon;
   if (lemon.tree === "Lemon") {
     icon = 'https://res.cloudinary.com/dujcpxlhk/image/upload/c_scale,w_25/v1592092413/Lemons/hiclipart.com_2.png'
@@ -19,7 +21,7 @@ const LemonIndexItem = ({lemon, setFocus, editLemon, deleteLemon}) => {
         <div className="lemon-location"> { lemon.location } </div>
         <div className="lemon-finder"> From: { lemon.finder } </div>
       </div>
-      { (sessionStorage.getItem("robertslemonpartykey") === lemon.token) && (<div className="lemon-actions">
+      { (sessionStorage.getItem("robertslemonpartykey") === lemon.token || editModeCheck) && (<div className="lemon-actions">
         <div onClick={() => editLemon(lemon.id)} className="mr-10">Edit</div>
         <div onClick={() => deleteLemon(lemon.id)}>Delete</div>
       </div>)}
