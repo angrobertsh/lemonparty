5e52362c08eef997c3f050523e077c8084f73a9c
angrobertsh <angrobertsh@gmail.com>
2020-07-19 23:06:41 -0700

    sheen

--- frontend/components/lemon_form/lemon_add_button.jsx
+++ frontend/components/lemon_form/lemon_add_button.jsx
@@ -3,20 +3,34 @@ import React from 'react';
 class LemonAddButton extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      sheen: true,
+    }
+  }
+
+  componentDidMount() {
+    this.timeoutId = setTimeout(function () {
+      this.setState({sheen: false});
+    }.bind(this), 10000);
+  }
+
+  componentWillUnmount () {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
+    }
   }
 
   render(){
-    if (!this.props.formOpen) {
-      return (
-        <div className="open-lemon-form-container">
-          <div className="open-lemon-form" onClick={this.props.toggleLemonForm}>
-            Add a Lemon Tree
-          </div>
+    const hide = !this.props.formOpen ? "" : "hide"
+    const sheen = this.state.sheen ? "sheen" : ""
+
+    return (
+      <div className={`open-lemon-form-container ${hide}`}>
+        <div className={`open-lemon-form ${sheen}`} onClick={this.props.toggleLemonForm}>
+          Add a Lemon Tree
         </div>
-      );
-    } else {
-      return null;
-    }
+      </div>
+    );
   }
 }
 
