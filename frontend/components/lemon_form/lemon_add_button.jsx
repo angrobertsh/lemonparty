b8b5a2b365040ce13c5026c7109930ebc9548547
angrobertsh <angrobertsh@gmail.com>
2020-07-28 11:10:22 -0700

    timeout and background text color

--- frontend/components/lemon_form/lemon_add_button.jsx
+++ frontend/components/lemon_form/lemon_add_button.jsx
@@ -11,7 +11,7 @@ class LemonAddButton extends React.Component{
   componentDidMount() {
     this.timeoutId = setTimeout(function () {
       this.setState({sheen: false});
-    }.bind(this), 5000);
+    }.bind(this), 7500);
   }
 
   componentWillUnmount () {
