a069af1facd70dd451d31aa2014cc9513fd5b81c
angrobertsh <angrobertsh@gmail.com>
2020-07-28 10:44:05 -0700

    image cleanup

--- frontend/components/lemon/lemon_background_changer.jsx
+++ frontend/components/lemon/lemon_background_changer.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const bgs = {
-  0: 'lemon-bg-8',
-  1: 'lemon-bg-9',
-  2: 'lemon-bg-7',
-  3: 'lemon-bg-6',
-  4: 'lemon-bg-6',
-  5: 'lemon-bg-5',
-  6: 'lemon-bg-4',
-  7: 'lemon-bg-3',
-  8: 'lemon-bg-2',
-  9: 'lemon-bg-1',
+  0: 'lemon-bg-1',
+  1: 'lemon-bg-1',
+  2: 'lemon-bg-3',
+  3: 'lemon-bg-3',
+  4: 'lemon-bg-4',
+  5: 'lemon-bg-4',
+  6: 'lemon-bg-5',
+  7: 'lemon-bg-5',
+  8: 'lemon-bg-8',
+  9: 'lemon-bg-8',
 }
 
 const bgClass = () => {
