3d75f8fd135bf21a1d3cd7d20fc9bca8998124b1
angrobertsh <angrobertsh@gmail.com>
2020-07-30 22:00:10 -0700

    reduce assets package

--- frontend/components/lemon/lemon_background_changer.jsx
+++ frontend/components/lemon/lemon_background_changer.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const bgs = {
-  0: 'lemon-bg-1',
-  1: 'lemon-bg-1',
+  0: 'lemon-bg-3',
+  1: 'lemon-bg-3',
   2: 'lemon-bg-3',
   3: 'lemon-bg-3',
-  4: 'lemon-bg-4',
-  5: 'lemon-bg-4',
-  6: 'lemon-bg-5',
-  7: 'lemon-bg-5',
+  4: 'lemon-bg-3',
+  5: 'lemon-bg-6',
+  6: 'lemon-bg-8',
+  7: 'lemon-bg-8',
   8: 'lemon-bg-8',
   9: 'lemon-bg-8',
 }
